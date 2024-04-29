403205ab67315ac76847c8150c404f924de17ef8
gabykarsten <karstengaby@gmail.com>
2024-04-29 13:03:22 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Gabriella Karsten and is{" "}
-        <a
-          href="https://github.com/gabykarsten/react-weatherapp"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Gabriella Karsten and is{" "}
+          <a
+            href="https://github.com/gabykarsten/react-weatherapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
