96b60da65e993fe6e73ba3aa5de7c8e2fe1c7189
gabykarsten <karstengaby@gmail.com>
2024-04-29 12:41:02 -0700

    added footer on app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Gabriella Karsten and is{" "}
+        <a
+          href="https://github.com/gabykarsten/react-weatherapp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
     </div>
   );
 }
